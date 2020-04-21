0a5d183f937804c319578fd287f5d498cdaa035b
Régis Soares <regis@regissoares.com>
2020-04-21 18:36:06 -0300

    Compilando bootstrap usando grunt

--- CustomizacaoBootstrap3/App_Start/BundleConfig.cs
+++ CustomizacaoBootstrap3/App_Start/BundleConfig.cs
@@ -8,23 +8,20 @@ namespace CustomizacaoBootstrap3
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                        "~/js/scripts.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/js/jquery.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/js/jquery.validate*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                        "~/js/bootstrap.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/css/styles").Include(
+                        "~/css/style.css"));
         }
     }
 }
---
+++ CustomizacaoBootstrap3/Gruntfile.js
@@ -0,0 +1,85 @@
+﻿/*
+This file in the main entry point for defining grunt tasks and using grunt plugins.
+Click here to learn more. https://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
+*/
+module.exports = function (grunt) {
+    grunt.initConfig({
+        copy: {
+            dist: {
+                files: [
+                    { expand: true, cwd: 'bower_components/jquery/dist/', src: ['**'], dest: 'js/' },
+                    { expand: true, cwd: 'bower_components/bootstrap/dist/fonts/', src: ['**'], dest: 'fonts/' },
+                    { expand: true, cwd: 'bower_components/bootstrap/dist/js/', src: ['**'], dest: 'js/' },
+                    { expand: true, cwd: 'bower_components/jquery-validation/dist/', src: ['**'], dest: 'js/' },
+                    { expand: true, cwd: 'bower_components/jquery-validation-unobtrusive/src/', src: ['**'], dest: 'js/' }
+                ]
+            }
+        },
+        less: {
+            options: {
+                ieCompat: true,
+                strictMath: true
+            },
+            dist: {
+                src: 'less/style.less',
+                dest: 'css/style.css'
+            }
+        },
+        postcss: {
+            options: {
+                processors: [
+                    require('autoprefixer')({ cascade: false })
+                ]
+            },
+            dist: {
+                src: 'css/style.css'
+            }
+        },
+        cssmin: {
+            options: {
+                compatibility: 'ie9',
+                level: {
+                    1: {
+                        specialComments: 'all'
+                    }
+                }
+            },
+            dist: {
+                src: 'css/style.css',
+                dest: 'css/style.min.css'
+            }
+        },
+        uglify: {
+            options: {
+                compress: true,
+                mangle: true,
+                ie8: true
+            },
+            dist: {
+                src: 'js/scripts.js',
+                dest: 'js/scripts.min.js'
+            }
+        },
+        watch: {
+            less: {
+                files: ['less/**/*.less'],
+                tasks: 'build:css'
+            },
+            js: {
+                files: ['js/**/*.js'],
+                tasks: 'uglify'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-postcss');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('build:css', ['less', 'postcss', 'cssmin']);
+    grunt.registerTask('build', ['copy', 'build:css', 'uglify']);
+    grunt.registerTask('default', ['build', 'watch']);
+};
